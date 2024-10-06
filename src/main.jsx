f0af92d343e78b57b3ae7238c4edfe057da7fd32
eid <eidhachem1@gmail.com>
2024-10-06 09:39:07 +0300

    Install and configure sentry

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,27 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React, { StrictMode } from "react"
+import { createRoot } from "react-dom/client"
+import App from "./App.jsx"
+import "./index.css"
+import * as Sentry from "@sentry/react"
 
-createRoot(document.getElementById('root')).render(
+Sentry.init({
+  dsn: "https://9502c348f1a6fe2b3314bac8a0a31c7b@o4508074315743232.ingest.de.sentry.io/4508074320789584",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.reactRouterV6BrowserTracingIntegration({
+      useEffect: React.useEffect,
+    }),
+    Sentry.replayIntegration(),
+  ],
+  // Tracing
+  tracesSampleRate: 1.0,
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0,
+})
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-  </StrictMode>,
+  </StrictMode>
 )
